f821445f079d7f40e36170f63dea193cbf7b38b5
Максим Орлов <ocmaxim97@gmail.com>
2019-04-04 11:57:55 +0300

    5.3

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # учебная программа
 a=int(input('Введите число A'))
 b=int(input('Введите число B'))
+print ('A+B = ', a+b)
 print ('2*A = ', 2*a)
-print ('A-B = ', a-b)
+print ('a-B = ', a-b)
+print ('A/B = ', a/b)
